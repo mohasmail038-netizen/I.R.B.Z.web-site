90112b7a43e0fd3df790e87192548796273446ce
mohasmail038-netizen <mohasmail038@gmail.com>
2026-02-06 11:36:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("✅ تم إرسال رسالتك بنجاح! شكراً لتواصلك مع اتحاد الزمالة.");
-  this.reset();
-});
 
